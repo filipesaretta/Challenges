e1da424cb05c8f2c0c71ca4697654551bdb2200e
filipesaretta <filipessm124@gmail.com>
2022-12-22 10:22:26 -0300

    Fix: punctuation is decrypted too

--- CaesarsCipher/index.js
+++ CaesarsCipher/index.js
@@ -1,34 +1,38 @@
-// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
-
-// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
-
-// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
-
-// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
-
 function rot13(str) {
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let cipher = ''
-  for (let i = 0;  i < str.length; i++) {
-    
-    const curr = str.charAt(i)
-    const letter = alphabet.indexOf(curr)
+  const key = alphabet.split('');
+  const strToArr = str.split('')
+  const reg = /[!?.\s]$/
+  let cipher = []
 
-    if(letter !== -1) {
-      let newI = letter - 13;
-      if(newI < 0) {
-        newI = letter + 13;
-        cipher += alphabet.charAt(newI)
-      } else {
-        newI = letter - 13;
-        cipher += alphabet.charAt(newI)
-      }
-      
+  for (let i = 0; i < str.length; i++) {
+    // Saves the punctuation to the respective positions
+    if (reg.test(strToArr[i])) {
+      cipher.push(strToArr[i])
     }
-  } 
-  
-  return  cipher 
-  
-}   
+    console.log(strToArr[i])
+
+    // Get the letter
+    const curr = str.charAt(i)
+    // Get the letter position in the key which is the alphabet 
+    const letter = key.indexOf(curr)
+
+    if(letter !== -1){
+      let decryptKey = (letter - 13)
+      // Check if the decryptKey is negative
+      if (decryptKey < 0) {
+        // If it is  reset to go the other way around
+        decryptKey = letter + 13
+        cipher.push(key[decryptKey])
+      } else {
+        cipher.push(key[decryptKey])
+      }
+    }
+  }
+
+  return cipher.join('')
+
+}
 
 console.log(rot13("SERR YBIR?"))
+
